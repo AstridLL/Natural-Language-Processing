1ee830c79b88045ac831dc161cdccee33aafd7fa
AstridLL <Astridll@astrids-mbp-3.eduroam.net.au.dk>
2020-09-04 12:40:37 +0200

    build function and used function on list of items

--- python_play1.py
+++ python_play1.py
@@ -6,20 +6,48 @@ print(astring[3:8:2]) #(start:stop:step)
 #%%
 
 print("hello")
-#emgs
 
 string = "hello there"
 print(string)
 
-
-import tensorflow as tf
+import nltk
+#import tensorflow as tf
 
 # lidt ekstra code 
 # lidt flere 
 # ændring
 
-import stanza
+#import stanza
 
-stanza.download("da")
+#stanza.download("da")
 
-stanza.download("en")
+#stanza.download("en")
+
+# %%
+names = ["Pia", "Ina", "Rebecca", "Lotte", "Ida", "Michelle"]
+jobs = ["stewardess", "cleaner", "consultant", "gamer", "athlete", "post"]
+hours = [24,37,45]
+#import random
+
+def namefunction(name):
+    if name in names:
+        namesnumber = names.index(name)
+        jobsnumber = jobs[namesnumber]
+        #return jobsnumber
+
+        if jobsnumber in jobs:
+            yourhours = random.choice(hours)
+            #return yourhours
+            print("So your name is %s, your job is %s, and you work %d hours a week." % (name, jobsnumber, yourhours))
+        else:
+            print("there is an error")
+    else:
+        print("The name you wrote did not match with any of the names in the nameslist")
+
+namefunction("Lotte")
+
+# loop through the entire list of names 
+for name in names:
+    namefunction(name)
+
+# %%
